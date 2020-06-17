987f36e8f9ee0530d50d0b0c8f80f5e699e4a37d
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-17 10:21:46 +0300

    refacoring to another approch with interface

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,21 +1,31 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
 import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class MatchReader extends CsvFileReader<MatchData> {
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
 
-  mapRow(row: string[]): MatchData { 
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      +row[3],
-      +row[4],
-      row[5] as MatchResult,
-      row[6],
-    ]
+export class MatchReader {
+  matches: MatchData[] = []; 
+  
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader
+      .data
+      .map((row: string[]): MatchData => [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        +row[3],
+        +row[4],
+        row[5] as MatchResult,
+        row[6],
+      ])
   }
 
 }
